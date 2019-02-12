cf33f64d0eecc582af73b27ca554a26679d51356
Aleksander <aleksadi@stud.ntnu.no>
2019-02-12 15:54:09 +0100

    Test commit

--- gitlab_tests.sh
+++ gitlab_tests.sh
@@ -1,3 +1,23 @@
 #!/usr/bin/env bash
+#echo "************************"
+#echo "******** Update ********"
+#echo "************************"
+#apt-get -y update
+#echo "*********************************"
+#echo "******** OS dependencies ********"
+#echo "*********************************"
+#apt-get -y install apt-utils
+#apt-get -y install python3-pip python3-dev python3-setuptools
+echo "*************************************"
+echo "******** Django dependencies ********"
+echo "*************************************"
+#pip install --upgrade pip
+pip install -r requirements.txt
+echo "***********************************"
+echo "******** Zone install step ********"
+echo "***********************************"
 python -V
 pip freeze
+echo "************************"
+echo "******** Finish ********"
+echo "************************"
