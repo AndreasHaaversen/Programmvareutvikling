17533f03f6c804ceae4139e5f0d205816d89a73c
Aleksander <aleksadi@stud.ntnu.no>
2019-02-12 15:38:54 +0100

    Test commit

--- gitlab_build.sh
+++ gitlab_build.sh
@@ -1,14 +1,22 @@
 #!/usr/bin/env bash
-echo "******** install ********"
+echo "*************************"
+echo "******** Install ********"
+echo "*************************"
 apt-get -y update
+echo "*********************************"
 echo "******** OS dependencies ********"
-apt-get -y install python3-pip
-apt-get -y install python3-dev python3-setuptools
-apt-get -y install git
-apt-get -y install supervisor
+echo "*********************************"
+#apt-get -y install apt-utils
+#apt-get -y install python3-pip python3-dev python3-setuptools
+echo "*************************************"
 echo "******** Django dependencies ********"
+echo "*************************************"
 pip install --upgrade pip
 pip install -r requirements.txt
-echo "******** zone install step ********"
+echo "***********************************"
+echo "******** Zone install step ********"
+echo "***********************************"
 pip freeze
-echo "******** finish ********"
+echo "************************"
+echo "******** Finish ********"
+echo "************************"
