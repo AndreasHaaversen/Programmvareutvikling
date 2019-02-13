e54c6804858fdc8f4deccbce93c1329aafb234e4
Aleksander <aleksadi@stud.ntnu.no>
2019-02-13 14:43:09 +0100

    Database test....

--- gitlab_tests.sh
+++ gitlab_tests.sh
@@ -2,27 +2,25 @@
 echo "************************"
 echo "******** Update ********"
 echo "************************"
-pacman -Syu python3 mariadb-clients --noconfirm
-#echo "*********************************"
-#echo "******** OS dependencies ********"
-#echo "*********************************"
-#apt-get install -y mysql-client
+apt-get -y update
+echo "*********************************"
+echo "******** OS dependencies ********"
+echo "*********************************"
+apt-get install -y mariadb-client
 echo "**********************************"
 echo "******** Pip dependencies ********"
 echo "**********************************"
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-python3 get-pip.py
-pip3 install --upgrade pip
-pip3 install -r requirements.txt
+#pip install --upgrade pip
+pip install -r requirements.txt
 echo "***********************************"
 echo "******** Zone install step ********"
 echo "***********************************"
 python -V
-pip3 freeze
+pip freeze
 echo "*************************************"
 echo "******** Database setup step ********"
 echo "*************************************"
-mysql -u root -pdev -h mysql -Bse "CREATE USER 'dev'@'localhost' IDENTIFIED BY 'dev';GRANT ALL PRIVILEGES ON *.* TO 'dev'@'localhost';"
+mysql -u root -pdev -h mariadb -Bse "CREATE USER 'dev'@'localhost' IDENTIFIED BY 'dev';GRANT ALL PRIVILEGES ON *.* TO 'dev'@'localhost';"
 python3 ./nigirifalls/manage.py migrate
 echo "*************************************"
 echo "******** Django default test ********"
