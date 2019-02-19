39c137c8226e5a04bbf3e6a0e1ad70b30163e8b9
Andreas Hammer Håversen <37153174+AndreasHaaversen@users.noreply.github.com>
2019-02-19 17:31:23 +0100

    Added choices for dish types to Dish model, completed index.html for this story.

--- nigirifalls/takeaway/models.py
+++ nigirifalls/takeaway/models.py
@@ -7,12 +7,20 @@ def get_image_path(instance, filename):
 
 
 class Dish(models.Model):
+    DISH_TYPE_CHOICES = (
+        ('maki', 'Maki'),
+        ('nigiri', 'Nigiri'),
+        ('sashimi', 'Sashimi'),
+        ('meny', 'Meny'),
+    )
+
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=200)
     image = models.ImageField(upload_to=get_image_path, height_field=None,
                               width_field=None, max_length=None)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    dish_type = models.CharField(max_length=25)
+    dish_type = models.CharField(max_length=7, choices=DISH_TYPE_CHOICES,
+                                 default='maki')
 
     def __str__(self):
         return self.name
