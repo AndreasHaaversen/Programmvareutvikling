4dc8943ed64dd5b025088bee9c1becfe00beb547
Andreas Hammer Håversen <37153174+AndreasHaaversen@users.noreply.github.com>
2019-02-23 15:43:42 +0100

    PEP8

--- nigirifalls/cart/views.py
+++ nigirifalls/cart/views.py
@@ -30,6 +30,6 @@ def cart_remove(request, dish_id):
 def cart_detail(request):
     cart = Cart(request)
     for item in cart:
-            item['update_quantity_form'] = CartAddDishForm(
-                initial={'quantity': item['quantity'], 'update': True})
+        item['update_quantity_form'] = CartAddDishForm(
+            initial={'quantity': item['quantity'], 'update': True})
     return render(request, 'cart/detail.html', {'cart': cart})
