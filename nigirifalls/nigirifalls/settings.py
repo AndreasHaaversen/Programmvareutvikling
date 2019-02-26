8617bcecd5df33e398aa55246d1ecd7fe283d710
Aleksander <aleksadi@stud.ntnu.no>
2019-02-26 14:01:27 +0100

    test deployment4

--- nigirifalls/nigirifalls/settings.py
+++ nigirifalls/nigirifalls/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 import socket
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -138,6 +137,3 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-# Django Heroku
-django_heroku.settings(locals())
