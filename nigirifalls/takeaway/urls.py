3e4fd30e766cc21933bb35bbe9326384f4fd99fb
Andreas Hammer Håversen <37153174+AndreasHaaversen@users.noreply.github.com>
2019-03-20 12:59:31 +0100

    Added confirmation email and PDF generation

--- nigirifalls/takeaway/urls.py
+++ nigirifalls/takeaway/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('checkout/', views.order_create, name='order_create'),
     path('thankyou/<int:pk>', views.ThankYouView.as_view(), name='thankyou'),
+    path('admin/order/<int:order_id>/pdf/',
+         views.create_order_pdf,
+         name='create_order_pdf'),
 ]
