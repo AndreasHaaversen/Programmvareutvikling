f68108286d51b3f7e5aa9bf3fe0bb89fa57c94db
Tuva Augustin Framnes <tuvaaf@stud.ntnu.no>
2019-03-20 13:17:00 +0100

    Almost finishetd search back end

--- nigirifalls/takeaway/urls.py
+++ nigirifalls/takeaway/urls.py
@@ -1,10 +1,11 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from . import views
 
 app_name = 'takeaway'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    re_path(r'^(?P<search>.*)/$', views.SearchView.as_view(), name = 'index2'),
     path('checkout/', views.order_create, name='order_create'),
     path('thankyou/<int:pk>', views.ThankYouView.as_view(), name='thankyou'),
 ]
--- nigirifalls/takeaway/views.py
+++ nigirifalls/takeaway/views.py
@@ -46,14 +46,17 @@ def order_create(request):
     return render(request, 'takeaway/checkout.html',
                   {'cart': cart, 'form': form})
 
-    def index_search_view(request):       
-        if request.method == 'GET': # this will be GET now      
-            dish_name =  request.GET.get('search') # do some research what it does       
-            try:
-                status = Add_prod.objects.filter(dishname__icontains=dish_name) # filter returns a list so you might consider skip except part
-            return render(request, 'search.html', {'dishes' :status})
-        else:
-            return render(request, 'search.html',{})
+class SearchView(generic.ListView):       
+    template_name = 'takeaway/index.html'
+
+    def get_queryset(self):
+        return Dish.objects.filter(dish_type__icontains=(self.kwargs['search'][9:])).order_by('dish_type')
+
+    def get_context_data(self):
+        context = super().get_context_data()
+        context['add_dish_form'] = CartAddDishForm()
+        context['cart'] = Cart(self.request)
+        return context
 
 
 
