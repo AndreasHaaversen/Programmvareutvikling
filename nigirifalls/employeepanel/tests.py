10ced50a2e63f90c8849efdcb278261ef4d2f4e2
Andreas Hammer Håversen <37153174+AndreasHaaversen@users.noreply.github.com>
2019-03-28 18:45:40 +0100

    Fixing tests, modified url patterns

--- nigirifalls/employeepanel/tests.py
+++ nigirifalls/employeepanel/tests.py
@@ -278,7 +278,7 @@ class EditOrderViewTests(TestCase):
                                          pickup_time=time,
                                          status='collected')
         id = str(order.id)
-        response = self.client.get('/employeepanel/order/' + id + '/edit/')
+        response = self.client.get('/employeepanel/order/edit/' + id +'/')
         self.assertEqual(response.status_code, 200)
 
     def test_view_url_accessible_by_name(self):
@@ -358,7 +358,7 @@ class EditOrderItemViewTest(TestCase):
                                              price=makedish.price,
                                              quantity=1)
         id = str(makeorder.id)
-        response = self.client.get('/employeepanel/order/' + id + '/edititem/')
+        response = self.client.get('/employeepanel/order/edititem/' + id +'/')
         self.assertEqual(response.status_code, 200)
 
     def test_view_url_accessible_by_name(self):
@@ -403,7 +403,8 @@ class AddOrderItemViewTests(TestCase):
                                          phone_number=46813998,
                                          pickup_time=time,
                                          status='collected')
-        response = self.client.get('/employeepanel/order/1/additem/')
+        id = str(order.id)
+        response = self.client.get('/employeepanel/order/additem/' + id + '/')
         self.assertEqual(response.status_code, 200)
 
     def test_view_url_accessible_by_name(self):
